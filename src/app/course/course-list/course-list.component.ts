dc4319bc098c23cedd338d4afa74c92168b19054
Marianna <marry-cherry-pie@mail.ru>
2021-04-18 14:54:37 +0300

    task-2 completed

--- src/app/course/course-list/course-list.component.ts
+++ src/app/course/course-list/course-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { COURSES } from '../mock-data';
 
 @Component({
   selector: 'app-course-list',
@@ -6,10 +7,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./course-list.component.scss']
 })
 export class CourseListComponent implements OnInit {
-
-  constructor() { }
+  courseList = COURSES;
 
   ngOnInit(): void {
+    console.log('OnInit')
+  }
+
+
+  public deleteCourse(item): void {
+    console.log('this course was delete:', item.id)
+  }
+
+  public editCourse(item): void {
+    console.log('this course was edite:', item.id)
+  }
+
+  public trackByFn(index: number): number {
+    return index;
   }
 
 }
